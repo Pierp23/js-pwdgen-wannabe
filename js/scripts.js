da77a6a3e9c0a8e8f056a9115b68aa59173bacda
Pierp23 <ryu.no.seishin.21@gmail.com>
2023-06-09 15:32:26 +0200

    Fine

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// PROMPT
+let firstName = prompt('Inserisci il tuo nome');
+
+let surname = prompt('Inserisci il tuo cognome');
+
+let color = prompt('Inserisci il tuo colore preferito');
+
+
+// DATI IN HTML
+document.getElementById('risults').innerHTML = firstName + surname + color + '101';
