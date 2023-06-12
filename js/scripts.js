f1f54cb1ffbd60de4d76651cd84aaa60a8a8f999
Pierp23 <ryu.no.seishin.21@gmail.com>
2023-06-12 10:15:11 +0200

    Stampa in console

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,10 @@ const surname = prompt('Inserisci il tuo cognome');
 
 const color = prompt('Inserisci il tuo colore preferito');
 
+// DATI IN CONSOLE
+console.log('firstName', firstName, typeof firstName);
+console.log('surname', surname, typeof surname);
+console.log('color', color, typeof color);
 
 // DATI IN HTML
 document.getElementById('risults').innerHTML = firstName + surname + color + '101';
